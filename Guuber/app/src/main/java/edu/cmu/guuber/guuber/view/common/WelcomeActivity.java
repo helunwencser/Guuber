d9d94992d97ce1a4aa18ec2f7ce1f498322638dc
helunwencser <lunwenh@andrew.cmu.edu>
2016-03-29 15:14:04 -0700

    demo for assignment 2

--- Guuber/app/src/main/java/edu/cmu/guuber/guuber/view/common/WelcomeActivity.java
+++ Guuber/app/src/main/java/edu/cmu/guuber/guuber/view/common/WelcomeActivity.java
@@ -1,5 +1,6 @@
-package edu.cmu.guuber.guuber;
+package edu.cmu.guuber.guuber.view.common;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import edu.cmu.guuber.guuber.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -18,14 +21,24 @@ public class WelcomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        /*
+        * read values
+        * */
+        String name = savedInstanceState.getString("name");
+        /**
+         * read values
+         * */
+
+        /**
+         * intent for screen flow
+         * */
+        //if there are multiple flows here, please use if else statement
+        Intent intent = new Intent(this, SignupActivity.class);
+        intent.putExtra("name", "bob");
+        this.startActivity(intent);
+        /**
+         * intent for screen flow
+         * */
     }
 
     @Override
