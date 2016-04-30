3cd1e495cc23ad115a57bfe7f255d30de83b7260
helunwencser <lwhecser@gmail.com>
2016-04-30 16:22:42 -0700

    add operation class

---
+++ Guuber/app/src/main/java/guuber/cmu/edu/messageConst/Operation.java
@@ -0,0 +1,14 @@
+package guuber.cmu.edu.messageConst;
+
+/**
+ * Created by helunwen on 4/30/16.
+ */
+/**
+ * This class defines all operations GuuberService will take
+ * */
+public class Operation {
+
+    public static final String STARTBACKGROUNDSERVICE = "STARTBACKGROUNDSERVICE";
+
+    public static final String SENDMESSAGE = "SENDMESSAGE";
+}
