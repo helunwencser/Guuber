5f22b106685617efb97033bf3b5c7d0641d9e05e
helunwencser <lwhecser@gmail.com>
2016-05-02 18:42:50 -0700

    update start server bash

--- Server/src/startServer.sh
+++ Server/src/startServer.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-mysql -u root -proot < ./create_table.sql
+
+#this shell script define for starting server
+
+echo -n "Enter the mysql username: "
+read username
+echo -n "Enter the mysql password: "
+read -s password
+
+#create mysql database
+mysql -u $username -p$password < ./create_table.sql
 killall -9 java
 find . -type f -name '*.class' -delete
 javac -cp ../lib/mysql-connector-java-5.1.38-bin.jar:. guuber/cmu/edu/server/Server.java
